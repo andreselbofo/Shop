71d137d1d2f79e580d03f960539233ab21323513
andres yepes <andreselbofo10@gmail.com>
2019-05-08 12:57:38 -0500

    cc

--- Shop.Web/Models/ResetPasswordViewModel.cs
+++ Shop.Web/Models/ResetPasswordViewModel.cs
@@ -5,7 +5,24 @@ using System.Threading.Tasks;
 
 namespace Shop.Web.Models
 {
+    using System.ComponentModel.DataAnnotations;
+
     public class ResetPasswordViewModel
     {
+        [Required]
+        public string UserName { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare("Password")]
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        public string Token { get; set; }
     }
+
 }
